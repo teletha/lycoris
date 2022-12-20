8c57bd0eca0f28d469232bdd9b99acd9e66ffc60
teletha <teletha@users.noreply.github.com>
2022-12-20 03:59:24 +0000

    deploy: 8c31c7eb498429d734075b9d5c2c07de6d1e8d07

---
+++ root.js
@@ -0,0 +1,23 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"lycoris"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Tree",
+			"packageName": "lycoris",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "TreeNode",
+			"packageName": "lycoris",
+			"type": "AbstractClass"
+		}
+	]
+}
